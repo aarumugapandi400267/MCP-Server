855b03cfe5a242ea06048b17bc89e0ba8e3a365b
Aarumugapandi <aarumugapandi400267@gmail.com>
2025-11-15 21:59:02 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+// Program.cs
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMcpServer()
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+var app = builder.Build();
+
+app.MapMcp();
+
+app.Run("http://localhost:3001");
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+
+    [McpServerTool, Description("Adds two numbers")]
+    public static int Add(int a, int b) => a + b;
+
+}
